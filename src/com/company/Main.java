9a71d5dd56d83d67a73d4e7211ea4a871993b149
TimØ <Tim_thorndal@hotmail.com>
2021-09-14 09:24:04 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int result;
+        //I haven't entered any value to this code. And there are no program for it to run.
+
+        System.out.println("boo!") + 7;
+        // Java: not a statement error will occur.
+
+
+    }
+}
